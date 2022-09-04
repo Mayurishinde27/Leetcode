73afda4e4350042987a004eff47c77425a18da86
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2022-09-04 18:50:09 +0530

    Time: 223 ms (16.29%), Space: 67.7 MB (90.59%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -6,19 +6,18 @@ public:
         int max_sum = 0;
         for(int i=0; i<nums.size(); i++)
         {
-            max_sum+=nums[i];
-        
-            if(sum<max_sum)
+            max_sum +=nums[i];
+            if(sum <= max_sum)
             {
                 sum = max_sum;
             }
-        
-            if(max_sum<0)
+            if(max_sum < 0)
             {
                 max_sum = 0;
             }
         }
         return sum;
+
         
     }
 };
