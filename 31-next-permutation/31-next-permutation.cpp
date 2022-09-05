eaa535714844bc572a6db05b7609d2548ed7c810
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2022-09-05 23:30:26 +0530

    Time: 4 ms (77.21%), Space: 12.2 MB (36.41%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums)
     {
+        //using c++ inbuilt function
         next_permutation(nums.begin(),nums.end());
         
     }
