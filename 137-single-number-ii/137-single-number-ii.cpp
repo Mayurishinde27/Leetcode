342e7200bc570ef861e9b5019b04a3169b6a34ab
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2022-09-18 19:48:04 +0530

    Time: 23 ms (14.72%), Space: 10.3 MB (5.81%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        map<int,int> m;
+        
+        for(auto x:nums)
+        {
+            m[x]++;
+        }
+        for(auto x:m)
+        {
+            if(x.second == 1)
+            {
+                return x.first;
+            }
+        }
+        return -1;
+        
+    }
+};
