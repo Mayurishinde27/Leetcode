8c2c88aba960c6cb6716c88c49e9d64c9cbd8b0b
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2022-10-25 20:57:55 +0530

    Time: 403 ms (5.15%), Space: 93.2 MB (88.93%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -7,8 +7,8 @@ public:
         int profit = 0;
         for(int i = 0; i<prices.size()-1; i++)
         {
-            minimum = min(minimum,prices[i]); // 7 1
-            profit = min(profit,minimum-prices[i+1]); // 0,1-5 = 6 
+            minimum = min(minimum,prices[i]);
+            profit = min(profit,minimum-prices[i+1]);  
             
             
         }
