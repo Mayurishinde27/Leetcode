e13faeea66a756c6d0844bd5d1e73d0266ea16e7
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2022-11-01 13:44:58 +0530

    Time: 26 ms (18.52%), Space: 18.2 MB (47.79%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -7,17 +7,10 @@ public:
         int count = 0;
         for(int i = 0, j = i; i<student.size(); i++ , j++)
         {
-            // for(int j = 0; j<seat.size(); j++)
-            // {
                 count += abs(student[i] - seat[j]); 
-            // }
+          
         }
         return count;
         
     }
-
-    
-    // seat = [1,4,5,9]    [2,2,6,6]
-    //stud =  [1,2,3,6]    [1,2,3,6]
-    // 2+2+3                // 1+0+3+0
 }; 
