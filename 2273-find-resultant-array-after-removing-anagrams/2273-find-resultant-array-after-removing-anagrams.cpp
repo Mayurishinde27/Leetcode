d55ab981d4c55b3654235548aca3ecc8f27831cb
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2022-11-05 23:02:19 +0530

    Time: 13 ms (83.87%), Space: 12.3 MB (87.58%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,66 +1,27 @@
 class Solution {
 public:
-    int isAnagram(string s1, string s2)
-    {
-        unordered_map<char,int> m1;
-        if(s1.size() != s2.size())
-        {
-            return 0;
-        }
-        
-        for(auto x:s1)
-        {
-            m1[x]++;
-        }
-        for(auto x: s2)
-        {
-            m1[x]--;
-            if(m1[x]<0)
-            {
-                return 0;
-            }
-        }
-        return 1;
-    }
     vector<string> removeAnagrams(vector<string>& words)
     {
-           vector<string> :: iterator it;
-           for(int i = 0; i< words.size() - 1; i++)
-           {
-               if(isAnagram(words[i],words[i+1]))
-               {   
-                   it = words.begin();
-                   words.erase(it + 1 + i--);
-                   // if both the words are anagram then delete the next word 
-                   // i.e (it+1) and decrease the index by 1 so that it will chrck 
-                   // from begining.
-               }
-           }
-        return words;
-
-        
-        //         vector<vector<string>> v;
-//         unordered_map<string,vector<string>> m;
-        
-//         for(int i = 0; i<words.size(); i++)
-//         {
-//             string s = words[i];
-//             sort(s.begin(),s.end());
-//             m[s].push_back(words[i]);
-//         }
-//         for(auto x:m)
-//         {
-//             v.push_back(x.second);
-//         }
-//         for(int i = 0; i<v.size(); i++)
-//         {
-//             vec.push_back(v[i][0]);
-//         }
-//         return vec;
-        
-        
-       
-       
-        
+        vector<string> v;
+        v.push_back(words[0]);
+        int j = 0;
+        for(int i = 1; i<words.size(); i++)
+        {
+            string a = words[i];
+            sort(a.begin(),a.end());
+            string b = v[j];
+            sort(b.begin(),b.end());
+            
+            if(a == b)
+            {
+                continue;
+            }
+            else
+            {
+                v.push_back(words[i]);
+                j++;
+            }
+        }
+        return v;
     }
 };
