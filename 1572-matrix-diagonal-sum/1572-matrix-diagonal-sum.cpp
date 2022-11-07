103a18ddc2c09c7b3e647e6604f46869cb844974
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2022-11-07 23:36:09 +0530

    Time: 37 ms (29.92%), Space: 11.3 MB (60.08%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -8,20 +8,10 @@ public:
         
         for(int i = 0 , j = i; i<maxr; i++,j++)
         {
-            // for(int j = i; j<maxc; j++)
-            // {
-            //     if(i==j)
-            //     {
-                    sum += mat[i][j]; 
-            //     }
-            //     else{
-            //         break;
-            //     }
-            // }
+            sum += mat[i][j]; 
+
         }
-        
-        // for(int i = 0; i<maxr; i++)
-        // {
+
         int i = 0;
             for(int j = maxc - 1 - i; j>=0; j--)
             {
