aa2db8688ef914be852450c62916935850d00202
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-01-08 22:15:01 +0530

    Time: 93 ms (80.00%), Space: 39.9 MB (40.00%) - LeetHub

---
+++ 2527-find-xor-beauty-of-array/2527-find-xor-beauty-of-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorBeauty(vector<int>& nums)
+    {
+        
+        for(int i = 1; i<nums.size(); i++)
+        {
+            nums[0] = nums[0] ^ nums[i]; 
+        }
+        return nums[0];
+    }
+};
