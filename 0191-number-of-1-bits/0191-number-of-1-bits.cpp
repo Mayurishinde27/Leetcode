72fb819243543fd58d94e60cc339704df2a22da5
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-02-13 21:38:36 +0530

    Time: 2 ms (55.07%), Space: 5.9 MB (93.07%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,9 +3,9 @@ public:
     int hammingWeight(uint32_t n)
     {
         int count = 0;
-        while(n!=0)  //1011
+        while(n)  //1011
         {
-            n = n&(n-1); 
+            n = n & (n-1); 
             count++;
             
         }
