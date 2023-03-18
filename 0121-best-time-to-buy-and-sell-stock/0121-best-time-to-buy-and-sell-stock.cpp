3866a1e5514840332f03e2cb549f475a82e112df
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-03-19 00:22:28 +0530

    Time: 133 ms (72.47%), Space: 93.3 MB (91.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,16 +3,14 @@ public:
     int maxProfit(vector<int>& prices)
     {
         int minimum = INT_MAX;
-        int maximum = INT_MIN;
         int profit = 0;
         for(int i = 0; i<prices.size()-1; i++)
         {
-            minimum = min(minimum,prices[i]);
-            profit = max(profit,prices[i+1]-minimum);  
+            minimum = min(minimum,prices[i]); 
+            profit = max(profit,prices[i+1]-minimum); 
             
             
         }
         return profit;
-        
     }
 };
