fdf597ae49ae7069c036b20c48577decd8722ed3
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-03-25 15:33:04 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (95.94%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -20,7 +20,7 @@ public:
         }
         if(p->val!=q->val)
         {
-            return 0;
+            return false;
         }
         return(isSameTree(p->left,q->left)&&isSameTree(p->right,q->right));
         
