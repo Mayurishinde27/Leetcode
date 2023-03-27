53944a1d6f1d567bf7df3daa61144e9ec04517e0
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-03-28 01:04:16 +0530

    Time: 12 ms (45.82%), Space: 10.1 MB (61.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
+//      int check(int i,int j,vector<vector<int>>& grid)
+//      {
+//          if(i == 0 && j == 0)
+//          {
+//              return grid[i][j];
+//          }
+//          if(i<0 || j<0)
+//          {
+//              return 1e8;
+//          }
+//          int up = check(i-1,j,grid)+grid[i][j];
+//          int left = check(i,j-1,grid)+grid[i][j];
+         
+        
+//          return min(up,left);
+//      }
+    int check(int i,int j,vector<vector<int>>& grid, vector<vector<int>>& dp)
+    {
+         if(i == 0 && j == 0)
+         {
+             return grid[i][j];
+         }
+         if(i<0 || j<0)
+         {
+             return 1e8;
+         }
+         if(dp[i][j] != -1)
+         {
+             return dp[i][j];
+         }
+         int up = check(i-1,j,grid,dp)+grid[i][j];
+         int left = check(i,j-1,grid,dp)+grid[i][j];
+         
+         dp[i][j] = min(up,left);
+         return dp[i][j];
+    }
+    
     int minPathSum(vector<vector<int>>& grid)
     {
         int maxr = grid.size();
         int maxc = grid[0].size();
-        vector<vector<int>> sum(maxr+1,vector<int>(maxc+1,INT_MAX));
-        sum[0][1] = 0;
-        // sum.resize(maxr+1,vector<int>(maxc+1,grid[0][0]));
-        
-        for(int i = 1; i<=maxr; i++)
-        {
-            for(int j = 1; j<=maxc; j++)
-            {
-                sum[i][j] = min(sum[i-1][j],sum[i][j-1])+grid[i-1][j-1];
-            }
-        }
-        
-        return sum[maxr][maxc];
-        //return res;
-        
+        vector<vector<int>> dp(maxr,vector<int>(maxc,-1));
+        // vector<int> v;
+    
+        return check(maxr-1,maxc-1,grid,dp);
+        // return check(0,0,grid,dp,v);
     }
 };
