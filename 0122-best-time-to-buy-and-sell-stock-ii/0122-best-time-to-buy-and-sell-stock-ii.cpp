e5787db84f32d0d91be8bfa5e308c7bf5c6821be
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-04-01 23:58:22 +0530

    Time: 6 ms (73.57%), Space: 13 MB (93.60%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        
+        int profit = 0;
+        for(int i = 0; i<prices.size()-1; i++)
+        {
+             profit += max(prices[i+1]-prices[i],0); 
+
+        }
+        return profit;
+
+    }
+};
