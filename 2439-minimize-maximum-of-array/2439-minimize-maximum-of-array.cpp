6e3e88376159e651d64a46b29f969c55f1538a9b
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-04-07 00:20:10 +0530

    Time: 139 ms (74.20%), Space: 71.4 MB (62.06%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -3,14 +3,12 @@ public:
    int minimizeArrayValue(vector<int>& nums)
    {
        long long result=0;
-       long long val = 0;
        long long store = 0;
        
        for(int i = 0; i<nums.size(); i++)
        {
-           store += nums[i];
-           val = ceil((store)/(i+1.0));
-           result = max(result,val);
+            store += nums[i];
+            result = max(result, (store + i) / (i + 1));
            
        }
        return result;
