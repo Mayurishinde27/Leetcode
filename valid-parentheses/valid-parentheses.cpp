d423569a754a59afab3e9d3df1fb250a013a23c8
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-04-08 20:21:24 +0530

    Time: 0 ms, Memory: 6.4 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        
+        for(int i = 0; i<s.size(); i++)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+                char ch = st.top(); 
+                st.pop();
+                if((s[i] == ')' && ch == '(' )|| (s[i] == '}' && ch == '{' ) ||(s[i] == ']' && ch == '['))
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+                
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        return false;
+
+    }
+};
