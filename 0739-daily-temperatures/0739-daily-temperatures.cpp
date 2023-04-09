e79495a0aaa4e1292ee7fd1f7278f6a7268194e2
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-04-09 13:56:12 +0530

    Time: 215 ms (51.23%), Space: 98.7 MB (82.76%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,64 +1,21 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temp)
+    vector<int> dailyTemperatures(vector<int>& temperatures)
     {
-        vector<int> v(temp.size(),0);
-        int sum = 0;
-        stack<int> st;
-        for(int i = temp.size()-1; i>=0; i--)
+        stack<pair<int,int>> st;
+        vector<int> v(temperatures.size(),0);
+        for(int i = temperatures.size()-1; i>=0; i-- )
         {
-            while(!st.empty() && temp[st.top()] <= temp[i])
+            while(!st.empty() && st.top().first<=temperatures[i])
             {
                 st.pop();
             }
             if(!st.empty())
             {
-                v[i] = st.top() - i;
+                v[i] = st.top().second - i;
             }
-            st.push(i);
+            st.push({temperatures[i],i});
         }
-         
-
         return v;
-//         vector<int> v;
-//         stack<int> st;
-//         int count;
-//         for(int i = 0; i<temp.size(); i++)
-//         {
-//             wh(!st.empty() && st.top()>temp[i])
-//             {
-                
-//             }
-//             count = 0;
-//             for(int j = i + 1; j<temp.size(); j++)
-//             {
-//                 count++;
-//                 if(temp[i] < temp[j])
-//                 {
-//                     v.push_back(count);
-//                     count = 0;
-//                     break;
-//                 }
-                
-//             }
-//             if(count!=0)
-//             {
-//                 v.push_back(0);
-//             }
-            
-            
-            
-//         }
-//         int p = v.size();
-//         int q = temp.size();
-//         int s = q - p;
-//         for(int i = 0; i<s; i++)
-//         {
-//             v.push_back(0);
-//         }
-//         return v;
-        
-        
-        
     }
 };
