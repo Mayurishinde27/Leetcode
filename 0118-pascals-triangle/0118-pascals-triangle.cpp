4baed986c915a19e6680e5bd1d9e7bd620612f3f
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-04-17 19:13:53 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (95.05%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,17 +5,13 @@ public:
         vector<vector<int>> v(numRows);
         for(int i = 0; i<numRows; i++)
         {
-            v[i].resize(i+1);
-            v[i][0] = 1;
-            v[i][i] = 1;
-            
+            v[i].resize(i+1,1);
             for(int j = 1; j<i; j++)
             {
                 v[i][j] = v[i-1][j-1] + v[i-1][j];
             }
         }
         return v;
-        
-        
+
     }
 };
