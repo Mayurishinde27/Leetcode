7b766c441b436fd8348208f3dae7fe2b98392aa6
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-05-18 23:57:45 +0530

    Time: 16 ms (70.99%), Space: 13.6 MB (89.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,24 +4,23 @@ public:
     {
         unordered_map<int,int> m;
         vector<int> v;
+        priority_queue<pair<int,int>> pq;
         for(auto x:nums)
         {
             m[x]++;
         }
-        priority_queue<pair<int,int>> pq;
         for(auto x:m)
         {
             pq.push({x.second,x.first});
         }
         
-        while(k)
+        while(k--)
         {
             v.push_back(pq.top().second);
             pq.pop();
-            k--;
         }
-
-        return v;
         
+        return v;
+
     }
 };
