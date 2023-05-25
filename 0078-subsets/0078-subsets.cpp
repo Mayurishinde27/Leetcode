147faec00349d3211818f50acd5bc79bad16f25b
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-05-25 13:22:58 +0530

    Time: 8 ms (13.92%), Space: 15.2 MB (37.85%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void subset(int idx, vector<int> &v, vector<int> nums,int n)
+    {
+        if(idx == n)
+        {
+            ans.push_back(v);
+            return;
+        }
+        // for taking the number
+        v.push_back(nums[idx]);
+        subset(idx+1,v,nums,n);
+        // for not taking the number
+        v.pop_back();
+        subset(idx+1,v,nums,n);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
+        vector<int> vec;
+        int n = nums.size();
+        subset(0,vec,nums,n);
+        return ans;
+        
+    }
+};
