8102f62930515312772721e1a9c3d050d9f10c7a
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-05-30 17:48:04 +0530

    Time: 4 ms (55.63%), Space: 7.8 MB (52.36%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,73 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    set<vector<int>> st;
-//     int isDuplicate(vector<int> a, vector<int> b)
-//     {
-//         if(a.size() != b.size())
-//         {
-//             return 0;
-//         }
-        
-//         for(int i = 0; i<a.size(); i++)
-//         {
-//             if(a[i] != b[i])
-//             {
-//                 return 0;
-//             }
-//         }
-//         return 1;
-//     }
-    void subset(int idx, vector<int> &v, vector<int> nums,int n)
+    
+    void subsets(int idx,vector<int> nums, vector<int>& v, int n)
     {
-        if(idx == n)
+        ans.push_back(v);
+        for(int i = idx; i<n; i++)
         {
-            if(st.find(v) == st.end())
+            if(i !=idx && nums[i] == nums[i-1])
             {
-                st.insert(v);
-                ans.push_back(v);
-                
+                continue;
             }
-            return;
+            v.push_back(nums[i]);
+            subsets(i+1,nums,v,n);
+            v.pop_back();
         }
-        // for taking the number
-        v.push_back(nums[idx]);
-        subset(idx+1,v,nums,n);
-        // for not taking the number
-        v.pop_back();
-        subset(idx+1,v,nums,n);
-        
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums)
     {
-        vector<int> vec;
         sort(nums.begin(),nums.end());
+        vector<int> v;
         int n = nums.size();
-        subset(0,vec,nums,n);
-        
-//         int count = 0;
-//         for(int i = 0; i<ans.size(); i++)
-//         {
-//             count = 0;
-//             for(int j = 0; j<unique.size(); j++)
-//             {
-//                 if(isDuplicate(ans[i],unique[j]) == 1)
-//                 {
-//                     count = 1;
-//                     break;   
-//                 }
-//             }
-            
-//             if(count == 0)
-//             {
-//                 unique.push_back(ans[i]);
-//             }
-//         }
-
-//          return unique;
-        
+        subsets(0,nums,v,n);
         return ans;
-        
     }
 };
