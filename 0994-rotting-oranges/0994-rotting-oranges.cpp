35154a7969777b5a86bec6ffed60252640245d5b
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-06-06 22:01:38 +0530

    Time: 7 ms (73.48%), Space: 13.1 MB (76.48%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    
+    int orangesRotting(vector<vector<int>>& grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        // vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q; // {{r,c},t}
+        int vis[n][m];
+        int time = 0;
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<m; j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    q.push({{i,j},0});
+                    vis[i][j] = 2;
+                }
+                else
+                    vis[i][j] = 0;
+            }
+        }
+              
+        int delrow[] = {-1,0,+1,0};
+        int delcol[] = {0,+1,0,-1};
+        
+        while(!q.empty())
+        {
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+
+            time = max(time,t);
+            q.pop();
+            for(int i = 0; i<4; i++)
+            {
+                int nRow = r + delrow[i];
+                int nCol = c + delcol[i];
+
+                if(nRow>=0 && nCol >=0 && nRow <n && nCol <m && grid[nRow][nCol] == 1 && vis[nRow][nCol]!=2)
+                {
+                    q.push({{nRow,nCol},time+1});
+                    vis[nRow][nCol] = 2;
+                }
+            }
+        }
+        
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<m; j++)
+            {
+                if(grid[i][j] == 1 && vis[i][j] != 2)
+                {
+                    return -1;
+                }
+            }
+        }
+    
+        return time;
+        
+        
+    }
+};
