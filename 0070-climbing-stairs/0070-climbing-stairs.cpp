2c68cd03dc9bc91dda1993ac6ac43cf48e6dcf76
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-07-14 20:20:38 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-//     int f(int n, vector<int> &dp)
-//     {
-            //memoization
-//         if(n == 0) return 1;
-//         if(n == 1) return 1;
-        
-//         if(dp[n] != -1) return dp[n];
-        
-//         int left = f(n-1,dp);
-//         int right = f(n-2,dp);
-        
-//         return dp[n] = left + right;
-        
-//     }
     int climbStairs(int n)
     {
-        // tabulation
         if(n == 0) return 1;
         if(n == 1) return 1;
-        vector<int> dp(n+1, -1);
         
-        dp[0] = 1;
-        dp[1] = 1;
-        
+        int prev2 = 1;
+        int prev = 1;
         for(int i = 2; i<=n; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            int cur_i = prev + prev2;
+            prev2 = prev;
+            prev = cur_i;
         }
-        return dp[n];
+        return prev;
  
     }
 };
