70c1d1b0510871ec805aae7bcc2f8cfc942bc30d
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-07-14 19:37:10 +0530

    Time: 3 ms (68.58%), Space: 6 MB (55.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -5,17 +5,18 @@ public:
     {
         // base case
         if(n <= 1) return n;
-        
-        vector<int> dp(n+1,-1);
-        dp[0] = 0;
-        dp[1] = 1;
+
+        int prev2 = 0;
+        int prev = 1;
         
         for(int i = 2; i<=n; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            int cur_i = prev + prev2;
+            prev2 = prev;
+            prev = cur_i;
         }
         
-        return dp[n];
+        return prev;
         
     }
 };
