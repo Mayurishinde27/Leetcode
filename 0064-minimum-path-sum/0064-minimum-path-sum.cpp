3cafabf8069106a1d5155c61d265e51955808137
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-07-19 16:48:41 +0530

    Time: 13 ms (39.87%), Space: 10.2 MB (14.23%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,50 +1,25 @@
 class Solution {
-public:
-//      int check(int i,int j,vector<vector<int>>& grid)
-//      {
-//          if(i == 0 && j == 0)
-//          {
-//              return grid[i][j];
-//          }
-//          if(i<0 || j<0)
-//          {
-//              return 1e8;
-//          }
-//          int up = check(i-1,j,grid)+grid[i][j];
-//          int left = check(i,j-1,grid)+grid[i][j];
-         
-        
-//          return min(up,left);
-//      }
-    int check(int i,int j,vector<vector<int>>& grid, vector<vector<int>>& dp)
-    {
-         if(i == 0 && j == 0)
-         {
-             return grid[i][j];
-         }
-         if(i<0 || j<0)
-         {
-             return 1e8;
-         }
-         if(dp[i][j] != -1)
-         {
-             return dp[i][j];
-         }
-         int up = check(i-1,j,grid,dp)+grid[i][j];
-         int left = check(i,j-1,grid,dp)+grid[i][j];
-         
-         dp[i][j] = min(up,left);
-         return dp[i][j];
-    }
     
+    int f(int i, int j,vector<vector<int>>& grid,vector<vector<int>>& dp)
+    {
+        if(i == 0 && j == 0) return grid[i][j];
+        if(i<0 || j < 0) return 1e8;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = grid[i][j] + f(i-1,j,grid,dp);
+        int left = grid[i][j] + f(i,j-1,grid,dp);
+        
+        return dp[i][j] = min(up,left);
+        
+    }
+public:
     int minPathSum(vector<vector<int>>& grid)
     {
-        int maxr = grid.size();
-        int maxc = grid[0].size();
-        vector<vector<int>> dp(maxr,vector<int>(maxc,-1));
-        // vector<int> v;
-    
-        return check(maxr-1,maxc-1,grid,dp);
-        // return check(0,0,grid,dp,v);
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,grid,dp);
+        
     }
 };
