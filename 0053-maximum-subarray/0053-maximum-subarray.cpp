838e64a50402f3d5b878602c8346f5a4a340a964
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-07-30 21:18:22 +0530

    Time: 86 ms (98.84%), Space: 67.7 MB (79.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,16 +2,41 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums)
     {
-        int n = nums.size();
-        int max_so_far = nums[0];
-        int maxi = nums[0];
         
-        for(int i = 1; i<n; i++)
+        // kadane's algorithm 
+        
+        int n = nums.size();
+        int max_sum = 0;
+        int maxi = INT_MIN;
+        
+        for(int i = 0; i<n; i++)
         {
-            max_so_far = max(nums[i],max_so_far + nums[i]);
-            maxi = max(maxi,max_so_far);
+            max_sum += nums[i];
+            
+            if(maxi < max_sum)
+            {
+                maxi = max_sum;
+            }
+            
+            if(max_sum < 0)
+            {
+                max_sum = 0;
+            }
+            
+            // maxi = max(maxi,max_sum);
         }
         return maxi;
+        
+//         int n = nums.size();
+//         int max_so_far = nums[0];
+//         int maxi = nums[0];
+        
+//         for(int i = 1; i<n; i++)
+//         {
+//             max_so_far = max(nums[i],max_so_far + nums[i]);
+//             maxi = max(maxi,max_so_far);
+//         }
+//         return maxi;
         
     }
 };
