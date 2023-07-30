1b4df0218eb40279d698ca8a1ebbe2f78c37311a
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-07-30 14:20:56 +0530

    Time: 15 ms (62.97%), Space: 5.9 MB (80.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,20 +1,18 @@
 class Solution {
+    int start = 0;
+    
+    bool solve(int idx, string str)
+    {
+        if(idx == str.size()) return true;
+        
+        return solve(idx+1,str) && str[start++] == str[idx];
+    }
+    
+    
 public:
     bool isPalindrome(int x) {
         string str = to_string(x);
-        int i = 0;
-        int j = str.size() - 1;
         
-        while(i < j)
-        {
-            if(str[i] != str[j])
-            {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        
-        return true;
+        return solve(0,str);
     }
 };
