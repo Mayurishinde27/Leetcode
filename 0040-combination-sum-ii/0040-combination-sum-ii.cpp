04646a99a2b7bd0343ae1c692855e14337b5f273
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-02 00:23:26 +0530

    Time: 7 ms (61.06%), Space: 13.5 MB (15.56%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -7,22 +7,16 @@ public:
 
         if(target == 0)
         {
-            ans.push_back(v);
+            ans.push_back(v);  // O(k)
             return;
         }
-            
+        
         for(int i = idx; i<n; i++)
         {
-            if(i>idx && candidates[i] == candidates[i-1])
-            {
-                continue;
-            }
-            if(candidates[i] > target)
-            {
-                break;
-            }
+            if(i > idx && candidates[i] == candidates[i-1]) continue;
+            if(target < candidates[i]) break;
             v.push_back(candidates[i]);
-            combination(i+1,target - candidates[i],candidates,v,n);
+            combination(i+1,target-candidates[i],candidates,v,n);
             v.pop_back();
         }
         
@@ -35,6 +29,10 @@ public:
         combination(0,target,candidates,v,n);
         return ans;
         
+        
+        // TC - 2^n x k  (when all elements are unique then number of subsequences are 2^n)
+        // k-> average length of every combination
+        // SC - k * x  (average length is k and there are x combinations)
     }
 };
 
