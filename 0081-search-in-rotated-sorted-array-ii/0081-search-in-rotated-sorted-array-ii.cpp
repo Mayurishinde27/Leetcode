7fdcc29e983e570b802e34d24d641a1d166a6110
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-01 21:05:06 +0530

    Time: 3 ms (93.07%), Space: 13.9 MB (49.35%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        
+        int low = 0;
+        int high = n - 1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            
+            if(nums[mid] == target){
+                return true;
+            }
+            
+            if(nums[low] == nums[mid] && nums[mid] == nums[high])
+            {
+                low++;
+                high--;
+                continue;
+            }
+            
+            // left part sorted
+            if(nums[low] <= nums[mid])
+            {
+                // checking if the target element is exist in the left range or not
+                if(target >= nums[low] && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            
+            // right part sorted
+            else
+            {
+                // checking if the target element is exist in the right range or not
+                if(target >= nums[mid] && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
