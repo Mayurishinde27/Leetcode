1edb12cb5d9b608dfdbd67fb7388c5789219b4ac
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-02 19:23:04 +0530

    Time: 164 ms (97.57%), Space: 24.2 MB (85.40%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4){
+        unordered_map<int,int> mp;
+        int count = 0;
+        for(auto a:nums1)
+        {
+            for(auto b:nums2)
+            {
+                mp[(a+b)]++;
+            }
+        }
+        
+        for(auto c: nums3)
+        {
+            for(auto d: nums4)
+            {
+                if(mp.find(-(c+d)) != mp.end())
+                {
+                    count += mp[-(c+d)];
+                }
+            }
+        }
+        return count;
+        
+    }
+};
