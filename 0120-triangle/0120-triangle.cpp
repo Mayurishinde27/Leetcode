e74151ef8b85cc84757299d26da583122b071f86
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-18 22:41:56 +0530

    Time: 7 ms (56.75%), Space: 8.6 MB (79.85%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,28 +1,31 @@
 class Solution {
-    // tabulation 
+    // space optimization
 public:
     int minimumTotal(vector<vector<int>>& triangle)
     {
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+
+        vector<int> front(n,0);
+        vector<int> curr(n,0);
         
         // base case
         for(int i = 0; i<n; i++)
         {
-            dp[n-1][i] = triangle[n-1][i];
+            front[i] = triangle[n-1][i];
         }
         
         for(int i = n-2; i>=0; i--)
         {
             for(int j = i; j>=0; j--)
             {
-                int down = triangle[i][j] + dp[i+1][j];
-                int dia = triangle[i][j] + dp[i+1][j+1];
+                int down = triangle[i][j] + front[j];
+                int dia = triangle[i][j] + front[j+1];
 
-                dp[i][j] =  min(down, dia);
+                curr[j] =  min(down, dia);
             }
+            front = curr;
         }
         
-        return dp[0][0];
+        return front[0];
     }
 };
