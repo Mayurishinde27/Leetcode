f7084c097c0955de2dddd8476fb373b031a0f470
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-19 14:13:29 +0530

    Time: 131 ms (89.28%), Space: 76.3 MB (81.78%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,23 +1,58 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights)
+    int largestRectangleArea(vector<int>& height)
     {
-        heights.push_back(0);
-        int n = heights.size();
-        int ans = 0;
-        stack<int> s;
+        int n = height.size();
+        int leftSmall[n];
+        int rightSmall[n];
+        
+        stack<int> st;
+        
+        // for computing left small
         
         for(int i = 0; i<n; i++)
         {
-            while(s.size() && heights[i] <= heights[s.top()])
+            while(!st.empty() && height[st.top()] >= height[i])
             {
-                int h = heights[s.top()];
-                s.pop();
-                int left = s.size() ? s.top() : -1;
-                ans = max(ans, (i - left - 1) * h);
+                st.pop();
             }
-            s.push(i);
+            
+            if(st.empty()) leftSmall[i] = 0;
+            else{
+                leftSmall[i] = st.top()+1;
+            }
+            st.push(i);
         }
+
+        while(!st.empty()){
+            st.pop();
+        }
+        // for computing right small
+        
+        for(int i = n-1; i>=0; i--)
+        {
+            while(!st.empty() && height[st.top()] >= height[i])
+            {
+                st.pop();
+            }
+            
+            if(st.empty()) rightSmall[i] = n-1;
+            else{
+                rightSmall[i] = st.top()-1;
+            }
+            st.push(i);
+        }
+        
+        int ans = 0;
+        
+        for(int i = 0; i<n; i++)
+        {
+            ans = max(ans, (rightSmall[i] - leftSmall[i] + 1) * height[i]);
+        }
+        
         return ans;
+        
+        // TC: O(N)+O(N)+O(N)+O(N)
+        // SC: O(N)+O(N)+O(N)
     }
 };
