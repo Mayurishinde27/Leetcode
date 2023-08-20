408024317a3c52e4336a2d367e39e4d1a7b122ca
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-20 14:55:02 +0530

    Time: 5 ms (21.57%), Space: 9.1 MB (41.89%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,51 @@
 class Solution {
+    
+    void swapGreater(vector<int>& arr1,vector<int>& arr2, int idx1, int idx2){
+        if(arr1[idx1] > arr2[idx2]){
+            swap(arr1[idx1], arr2[idx2]);
+        }
+    }
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+    void merge(vector<int>& arr1, int m, vector<int>& arr2, int n)
     {
-        int left = m-1;
-        int right = n-1;
-        int last = m + n - 1;
-        while(left >=0 && right >=0 )
-        {
-            if(nums1[left] > nums2[right])
+        int len = (m+n);
+        int gap = (len)/2 + len % 2;
+        
+        while(gap > 0){
+            int left = 0;
+            int right = left + gap;
+            while(right<len)
             {
-                nums1[last] = nums1[left];
-                last--;
-                left--;
+                // arr1 & arr2
+                if(left < m && right>= m){
+                    swapGreater(arr1, arr2, left, right - m);
+                }
+                
+                // arr2 & arr2
+                else if(left >= m)
+                {
+                    swapGreater(arr2, arr2, left - m, right - m);
+                }
+                
+                // arr1 & arr1
+                else{
+                    swapGreater(arr1, arr1, left, right);
+                }
+                
+                left++;
+                right++;
             }
-            else{
-                nums1[last] = nums2[right];
-                last--;
-                right--;
-            }
+            
+            if(gap == 1) break;
+            gap = gap/2 + gap%2;
         }
-        while(right >=0 ){
-            nums1[last] = nums2[right];
-            last--;
-            right--;
+        
+        int idx = 0;
+        
+        for(int i = m; i<len; i++){
+            arr1[i] = arr2[idx++];
         }
+        // TC: O ( log(m+n) * (m+n) )
+        // SC: O(1)
     }
 };
