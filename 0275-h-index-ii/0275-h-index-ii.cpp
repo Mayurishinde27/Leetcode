c8a673bc2a15b9e73f36d978bec5797ad7522255
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-21 14:54:18 +0530

    Time: 19 ms (48.51%), Space: 18.5 MB (65.31%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -5,17 +5,26 @@ public:
         int n = citations.size();
         
         if(n == 1 && citations[0] > 0) return 1; 
-        int hIndex = n;
-      
-        for(int i = 0; i<n; i++)
+        int hIndex = 0;
+        
+        int i = 0;
+        int j = n-1;
+        
+        while(i<=j)
         {
-            if((citations[i] >= hIndex))
+            int mid = i + (j-i)/2;
+            if(citations[mid] >= n-mid)
             {
-                return hIndex;
+                hIndex = n - mid;
+                j = mid - 1;
             }
             
-            hIndex--;
+            else{
+                i = mid + 1;
+            }
+            
         }
+        
         return hIndex;
         
     }
