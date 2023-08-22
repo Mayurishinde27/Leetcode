2b3507667b946b2a4fec90773d61e58ce7ae5476
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-22 18:41:56 +0530

    Time: 8 ms (87.19%), Space: 10.9 MB (24.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,17 +4,20 @@ public:
     {
         vector<pair<int,int>> v;
         
-        for(int i = 0; i<nums.size(); i++)
+        //O(N)
+        for(int i = 0; i<nums.size(); i++)  
         {
-            v.push_back({nums[i],i});
+            v.push_back({nums[i],i});  // num,idx
         }
         
-        sort(v.begin(),v.end());
+        sort(v.begin(),v.end());  // O(NlogN)
         
         int i = 0;
         int j = nums.size()-1;
         
         vector<int> ans;
+        
+        // O(N)
         while(i<j)
         {
             if(v[i].first + v[j].first == target)
@@ -35,7 +38,8 @@ public:
         
         return ans;
         
-        
+        ///TC: ~ O(NlogN)
+        //SC : O(N)
         
     }
 };
