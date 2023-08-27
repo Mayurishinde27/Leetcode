4be503848c4997768abc2eb46eef38d10d523711
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-27 21:07:21 +0530

    Time: 51 ms (93.85%), Space: 234.9 MB (17.33%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,4 +1,23 @@
 class Solution {
+    
+    int solve(int pos, int jump, vector<int>&stones, unordered_map<int,int>&mp,vector<vector<int>> &dp)
+    {
+        // base case
+        if(pos == stones.size()-1){
+            return true;
+        }
+        
+        int val = stones[pos] + jump;
+        
+        if(pos >= stones.size() || jump <= 0 || mp.find(val) == mp.end()){
+            return false;
+        }
+        
+        if(dp[pos][jump] != -1) return dp[pos][jump];
+
+        return dp[pos][jump] = solve(mp[val],jump-1,stones,mp,dp) || solve(mp[val],jump,stones,mp,dp) || solve(mp[val],jump+1,stones,mp,dp);
+
+    }
 public:
     bool canCross(vector<int>& stones)
     {
@@ -10,23 +29,5 @@ public:
             mp[stones[i]] = i;
         }
         return solve(0,1,stones,mp,dp);
-    }
-    
-    int solve(int pos, int jump, vector<int>&stones, unordered_map<int,int>&mp,vector<vector<int>> &dp)
-    {
-        
-        if(pos == stones.size()-1){
-            return true;
-        }
-        int val = stones[pos] + jump;
-        
-        if(dp[pos][jump] != -1) return dp[pos][jump];
-        
-        if(pos >= stones.size() || jump <= 0 || mp.find(val) == mp.end()){
-            return false;
-        }
-        
-        return dp[pos][jump] = solve(mp[val],jump-1,stones,mp,dp) || solve(mp[val],jump,stones,mp,dp) || solve(mp[val],jump+1,stones,mp,dp);
-
     }
 };
