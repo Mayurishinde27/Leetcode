f14666ccc108bb15d83b6b13445c40489cbfdb63
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-30 20:28:06 +0530

    Time: 29 ms (45.19%), Space: 25.5 MB (39.48%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     void rotate(vector<int>& arr, int d)
     {
-        int n = arr.size();
-        d = d % n;
-        reverse(arr.begin(), arr.end());
-        reverse(arr.begin(), arr.begin()+d);
-        reverse(arr.begin()+d, arr.end());
+        // int n = arr.size();
+        // d = d % n;
+        // reverse(arr.begin(), arr.end());
+        // reverse(arr.begin(), arr.begin()+d);
+        // reverse(arr.begin()+d, arr.end());
        
         
-//         vector<int> left;
-//         int n = arr.size();
-//         d = d % n;
-//         for(int i = 0; i<n-d; i++)
-//         {
-//             left.push_back(arr[i]);
-//         }
+        vector<int> left;
+        int n = arr.size();
+        d = d % n;
+        for(int i = 0; i<n-d; i++)
+        {
+            left.push_back(arr[i]);
+        }
         
-//         // the second part of array to rotate
-//         for(int i = n-d; i<n; i++)
-//         {
-//             arr[i-(n-d)] = arr[i];
-//         }
+        // the second part of array to rotate
+        for(int i = n-d; i<n; i++)
+        {
+            arr[i-(n-d)] = arr[i];
+        }
         
-//         for(int i = d; i<n; i++)
-//         {
-//             arr[i] = left[i-d];
-//         }
+        for(int i = d; i<n; i++)
+        {
+            arr[i] = left[i-d];
+        }
+        
+        //TC: O(N)
+        // SC : O(d)
         
     }
 };
