89cb8638a1430ba72fab5ea6466ae513bc677697
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-30 23:06:00 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (82.69%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,19 +1,20 @@
 class Solution {
     
-    int solve(int n, vector<int> &nums,int target,vector<int>&dp)
+    int solve(vector<int> &nums,int target,vector<int>&dp)
     {
         if(target == 0)
         {
             return 1;
         }
         if(dp[target] != -1) return dp[target];
+        
         int count = 0;
         
-        for(int i = 0; i<n; i++)
+        for(int i = 0; i<nums.size(); i++)
         {
             if(nums[i] <= target)
             {
-                count += solve(n, nums, target-nums[i],dp);
+                count += solve(nums, target-nums[i],dp);
             }
         }
         dp[target] = count;
@@ -22,9 +23,8 @@ class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target)
     {
-        int n = nums.size();
         vector<int> dp(target+1,-1);
-        return solve(n,nums,target,dp);
+        return solve(nums,target,dp);
         
     }
 };
