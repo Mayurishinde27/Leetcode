0477c2f9311dc24b5ae6529c8a35f30efaba80f5
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-08-30 21:44:59 +0530

    Time: 7 ms (79.95%), Space: 19.8 MB (67.84%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -10,9 +10,8 @@
  * };
  */
 class Solution {
-    vector<vector<int>> ans;
 public:
-    void hasPathSum(TreeNode* root, int targetSum,vector<int>& v)
+    void hasPathSum(TreeNode* root, int targetSum,vector<int>& v,vector<vector<int>> &ans)
     {
         if(root == NULL)
         {
@@ -23,8 +22,8 @@ public:
         {
             ans.push_back(v);
         }
-        hasPathSum(root->left,targetSum-root->val,v);
-        hasPathSum(root->right,targetSum-root->val,v);
+        hasPathSum(root->left,targetSum-root->val,v,ans);
+        hasPathSum(root->right,targetSum-root->val,v,ans);
         
         v.pop_back();
    
@@ -32,7 +31,8 @@ public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum)
     {
         vector<int> v;
-        hasPathSum(root,targetSum,v);
+        vector<vector<int>> ans;
+        hasPathSum(root,targetSum,v,ans);
         return ans;
     }
 };
