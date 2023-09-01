4a45e816839318703204f869d808e6f43297bfab
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-09-01 23:51:57 +0530

    Time: 199 ms (62.47%), Space: 131 MB (19.51%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k)
+    {
+        int n = nums.size();
+        if (n < 2) return false;
+        
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        
+        int curr = 0;
+        for(int i = 0; i<n; i++)
+        {
+            curr += nums[i];
+            if(k != 0){
+                curr = curr % k;
+            }
+            
+            if(mp.find(curr) != mp.end()){
+                if(i - mp[curr] >= 2){
+                    return true;
+                }
+            }
+            else{
+                mp[curr] = i;
+            }
+        }
+
+        return false;
+    }
+};
