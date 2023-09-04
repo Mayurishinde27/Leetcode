88ef034c5151b887bfb12f3a3ae6fc2f982db840
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-09-04 23:34:17 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (65.03%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,14 +2,36 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums)
     {
-        int k = 1;
-        do{
-            if(k == 0) break;
-            k--;
-        }while(next_permutation(nums.begin(),nums.end()));
+        int n = nums.size();
+        int idx1 = -1;
         
-        if(k == 1){
-            sort(nums.begin(),nums.end());
+        // find idx1 -step1
+        for(int i = n-2; i>=0; i--)
+        {
+            if(nums[i] < nums[i+1])
+            {
+                idx1 = i;
+                break;
+            }
+        }
+        
+        // if there is no break point
+        if(idx1 == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        else{
+            // find idx2;  -step2
+            for(int idx2 = n-1; idx2>idx1; idx2--)
+            {
+                if(nums[idx2] > nums[idx1])
+                {
+                    swap(nums[idx1],nums[idx2]); // step3
+                    break;
+                }
+            }
+            
+            reverse(nums.begin()+idx1+1, nums.end()); // step4
         }
         
     }
