958bbc13b6a354d1224c2d9d697f04a326d0134a
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-09-05 14:18:00 +0530

    Time: 4 ms (54.39%), Space: 16.1 MB (21.51%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,23 +1,9 @@
 class Solution {
-//     string convert(stack<char>&st)
-//     {
-//         string str = "";
-//         while(!st.empty())
-//         {
-//             str += st.top();
-//             st.pop();
-//         }
-        
-//         reverse(str.begin(),str.end());
-//         return str;
-        
-//     }
     void solve(int open, int close,int n,string str,vector<string>&ans)
     {
         // base case
         if(str.size() == 2*n)
         {
-            // string str = convert(st);
             ans.push_back(str);
             return;
         }
@@ -39,35 +25,12 @@ class Solution {
         }
     }
 
-//     void solve(string str , int open, int close,int n,vector<string>&ans)
-//     {
-//         // base case
-//         if(open == close == n)
-//         {
-//             // string str = convert(st);
-//             ans.push_back(str);
-//             return;
-//         }
-        
-//         // check if we can add open parantheses
-//         if(open < n)
-//         {
-//             solve(str +'(' , open+1, close, n, ans);
-//         }
-        
-//         // check if we can add close paranthese
-//         if(close < open)
-//         {
-//             solve(str+ ')', open, close+1, n, ans);
-//         }
-//     }
 public:
     vector<string> generateParenthesis(int n)
     {
         vector<string> ans;
         string str = "";
         solve(0,0,n,str,ans);
-        // solve(str,0,0,n,ans);
         return ans;
         
     }
