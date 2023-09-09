daf00a1c45f93bb47f9e86359f7847178449cd0f
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-09-09 17:28:53 +0530

    Time: 3 ms (45.29%), Space: 6.4 MB (27.06%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    stack<char> st;
+    int minAddToMakeValid(string s)
+    {
+        int count = 0;
+        for(int i = 0; i< s.size(); i++)
+        {
+            if(s[i] == '(') 
+            {
+                st.push(s[i]);
+            }
+            else if(s[i] == ')' && (st.empty() || st.top() != '('))
+            {
+                count++;
+            }
+            else
+            {
+                st.pop();
+            }
+            
+        }
+        
+        return count + st.size();
+        
+//         if(count == 0)
+//         {
+//             return st.size();
+//         }
+//         return count;
+        
+        
+    }
+};
