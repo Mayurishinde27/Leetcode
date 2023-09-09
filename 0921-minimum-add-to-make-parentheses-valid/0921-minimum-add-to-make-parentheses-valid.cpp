ad3b85c17a82c054c3eb8c1cb1078818746d4ba3
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-09-09 17:37:27 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (69.76%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-    stack<char> st;
     int minAddToMakeValid(string s)
     {
-        int count = 0;
-        for(int i = 0; i< s.size(); i++)
+        stack<char> st;
+        int cnt = 0;
+        for(int i = 0; i<s.size(); i++)
         {
-            if(s[i] == '(') 
-            {
+            if(s[i] == '('){
                 st.push(s[i]);
             }
-            else if(s[i] == ')' && (st.empty() || st.top() != '('))
-            {
-                count++;
+            else{
+                if(!st.empty() && s[i] == ')' && st.top() == '('){
+                    st.pop();
+                }
+                else{
+                    cnt++;
+                }
             }
-            else
-            {
-                st.pop();
-            }
-            
         }
         
-        return count + st.size();
+//         for(int i = 0; i< s.size(); i++)
+//         {
+//             if(s[i] == '(') 
+//             {
+//                 st.push(s[i]);
+//             }
+//             else if(s[i] == ')' && (st.empty() || st.top() != '('))
+//             {
+//                 count++;
+//             }
+//             else
+//             {
+//                 st.pop();
+//             }
+            
+//         }
+        
+        return cnt + st.size();
         
     }
 };
