c226f17cf01bccd753ecfd7cd1ac8a4d8bb38858
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-09-10 19:11:02 +0530

    Time: 2 ms (56.67%), Space: 5.9 MB (71.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,21 +1,28 @@
 class Solution {
     int mod = 1e9+7;
 public:
-    int countOrders(int n)
+    
+    long long int f(int n)
     {
         if(n == 1) return 1;
+         return (long long)((n*2)-1) * ((long long)countOrders(n-1)*n) % mod;
+    }
+    int countOrders(int n)
+    {
+        // if(n == 1) return 1;
+        return f(n);
         
-        long long int previous = 1;
-        long long int ans = 1;
+//         long long int previous = 1;
+//         long long int ans = 1;
         
-        for(auto i = 2; i<=n; i++)
-        {
-            ans = (((i*2)-1) * previous*i) % mod;
-            previous = ans;
-        }
+//         for(auto i = 2; i<=n; i++)
+//         {
+//             ans = (((i*2)-1) * previous*i) % mod;
+//             previous = ans;
+//         }
         
-        return ans;
-        // return ((n*2)-1) * (countOrders(n-1)*n);
+//         return ans;
+        // return (long long)((n*2)-1) * (countOrders(n-1)*n) % mod;
     }
 };
 
