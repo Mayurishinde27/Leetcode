572a36778e26e413706f2cdce632ac59fca834e1
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-09-10 19:07:28 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.30%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int mod = 1e9+7;
+public:
+    int countOrders(int n)
+    {
+        if(n == 1) return 1;
+        
+        long long int previous = 1;
+        long long int ans = 1;
+        
+        for(auto i = 2; i<=n; i++)
+        {
+            ans = (((i*2)-1) * previous*i) % mod;
+            previous = ans;
+        }
+        
+        return ans;
+        // return ((n*2)-1) * (countOrders(n-1)*n);
+    }
+};
+
+// n - 1
+// n = 2  ((2*2)-1) * 1*2  = 6
+// n = 3  ((3*2) - 1) * 6 * 3
+
+// ((n*2)-1) * (f(n-1)*n)
