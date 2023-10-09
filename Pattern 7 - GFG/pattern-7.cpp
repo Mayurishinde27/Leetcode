a8bcc8598f2a8ab312f0aaa5d8d60da1a0296a90
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-10-09 22:48:38 +0530

    Added solution - LeetHub

--- Pattern 7 - GFG/pattern-7.cpp
+++ Pattern 7 - GFG/pattern-7.cpp
@@ -10,13 +10,22 @@ class Solution {
         // code here
         for(int i = 0; i<n; i++)
 	    {
+	        //space
 	        for(int j = 0; j<n-i-1; j++)
 	        {
 	            cout<<" ";
 	        }
+	        
+	        // star
 	        for(int j = 0; j<i*2+1; j++)
 	        {
 	            cout<<"*";
+	        }
+	        
+	        // space
+	        for(int j = 0; j<n-i-1; j++)
+	        {
+	            cout<<" ";
 	        }
 	        cout<<endl;
 	    }
