b7d1c0b193cf5b679641eefb758ff7459be24224
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-10-10 19:56:22 +0530

    Added solution - LeetHub

---
+++ Pattern 20 - GFG/pattern-20.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i = 1; i<n; i++)
+        {
+            // star
+            for(int j = 0; j<i; j++)
+            {
+                cout<<"*";
+            }
+            
+            // space
+            for(int j = 2*n-2*i; j>0; j--)
+            {
+                cout<<" ";
+            }
+            
+            //star
+            for(int j = 0; j<i; j++)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+            
+        }
+            for(int i = 0; i<n; i++)
+        {
+            // star
+            for(int j = 0; j<=n-i-1; j++)
+            {
+                cout<<"*";
+            }
+            
+            // space
+            for(int j = 0; j<2*i; j++)
+            {
+                cout<<" ";
+            }
+            
+            //star
+            for(int j = 0; j<=n-i-1; j++)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+            
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
