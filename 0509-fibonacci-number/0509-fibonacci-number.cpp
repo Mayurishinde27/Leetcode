17655938329ad54369ebed906ccc1cea554d7fa4
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-10-11 19:44:28 +0530

    Time: 12 ms (39.65%), Space: 6.3 MB (31.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-
     int fib(int n)
     {
-        // base case
-        if(n <= 1) return n;
-
-        int prev2 = 0;
-        int prev = 1;
-        
-        for(int i = 2; i<=n; i++)
+        if(n <= 1)
         {
-            int cur_i = prev + prev2;
-            prev2 = prev;
-            prev = cur_i;
+            return n;
         }
-        
-        return prev;
+        return fib(n-1) + fib(n-2);
         
     }
 };
